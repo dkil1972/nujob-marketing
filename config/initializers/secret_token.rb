53aa6aa2ec151eba130890a4540798bdae5c4a4c
ravalikaguntuku <ravalikaguntuku88@gmail.com>
2013-10-23 01:14:58 +0530

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NujobMarketing::Application.config.secret_key_base = '127c1f01d2ac8ac211dbaeff173c80ec2fd1740d6548e2a04b920062397dc927b647cd9470ff4be01bbb2a2393ffa23588e7d823ccb423b82a80862463d3f51a'
