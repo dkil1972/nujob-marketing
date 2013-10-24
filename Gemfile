72cf5196100158d7dbf7d42467205182899519a1
ravalikaguntuku <ravalikaguntuku88@gmail.com>
2013-10-24 22:14:40 +0530

    init

--- Gemfile
+++ Gemfile
@@ -4,7 +4,9 @@ source 'https://rubygems.org'
 gem 'rails', '4.0.0.rc2'
 ruby '1.9.3'
 gem 'haml-rails'
+group :production do 
 gem 'rails_12factor'
+end
 # Use sqlite3 as the database for Active Record
 gem 'pg'
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ NujobMarketing::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-  config.assets.compress = false
+  
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -60,7 +60,7 @@ NujobMarketing::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  # config.assets.precompile += [ '*.js', '*.css']
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
