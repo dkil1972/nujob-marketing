1c3aa92b0bf0170a25142296deb9eeaff4fccd88
ravalikaguntuku <ravalikaguntuku88@gmail.com>
2013-11-27 23:35:33 +0530

    init

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -9,8 +9,8 @@ def index
   require "uri"
   uri = URI.parse("http://api.osmek.com/feed")
   response = Net::HTTP.post_form(uri, {"api_key" => "8ytjaEzRiIrPl1meNC5UTfpnhO7vWKYB", "bin_id" => "8948" , "format" => "html"})
-  render   inline: response.body
-
+  
+  $res = response.body
 end
 
 end
