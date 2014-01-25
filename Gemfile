12e96a41a69b3f3b3fc53ff666097441d13032c7
ravalikaguntuku <ravalikaguntuku88@gmail.com>
2014-01-26 01:52:49 +0530

    init

--- Gemfile
+++ Gemfile
@@ -16,7 +16,7 @@ gem 'sass-rails', '~> 4.0.0.rc2'
 gem 'closure-compiler'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
-
+gem 'will_paginate', '~> 3.0'
 # Use CoffeeScript for .js.coffee assets and views
 gem 'coffee-rails', '~> 4.0.0'
 
--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,16 +1,39 @@
 class BlogController < ApplicationController
 
 
-
-
-def index
-
   require "net/http"
   require "uri"
+  require 'json'
   uri = URI.parse("http://api.osmek.com/feed")
-  response = Net::HTTP.post_form(uri, {"api_key" => "8ytjaEzRiIrPl1meNC5UTfpnhO7vWKYB", "bin_id" => "8948" , "format" => "html"})
-  
+  response = Net::HTTP.post_form(uri, {"api_key" => "8ytjaEzRiIrPl1meNC5UTfpnhO7vWKYB", "bin_id" => "8948" , "format" => "json"})
+  $blog_entries = JSON.parse(response.body)
   $res = response.body
+  
+def index
+
+  
+  require 'will_paginate/collection'
+  @posts= $blog_entries['items']
+
+  
+  current_page=(params[:page] ||= 1).to_i
+   
+   @page_results = WillPaginate::Collection.create(current_page, 5,@posts.length) do |pager|
+   pager.replace(@posts.to_a[(current_page-1)*5, current_page* 5])
+   end
+   
+end
+
+
+
+def show
+@posts= $blog_entries['items']
+for post in @posts
+if post['url_title'] == params[:id]
+$res=post
+end
+end
+
 end
 
 end
