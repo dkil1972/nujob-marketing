7ebcb349821c0f9e0f7b14649cd91945c8e698a2
Andy Davis <github@andy-davis.co.uk>
2014-05-14 19:02:23 +0100

    moved helper method to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery
+  
+  helper_method :content_item
+
+  def content_item(page, key)
+     item = ContentItem.find_or_create_by(:key => key, :page => page)
+     
+     if (item.text != nil)
+       return item.text
+     end
+     
+     first_time_edit_link = "<a href='/content_items/" + item._id + "/edit'>Click to replace</a>" 
+     return first_time_edit_link.html_safe
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,4 @@
 class HomeController < ApplicationController
-  helper_method :content_item
-
-  def content_item(page, key)
-     item = ContentItem.find_or_create_by(:key => key, :page => page)
-     
-     if (item.text != nil)
-       return item.text
-     end
-     
-     first_time_edit_link = "<a href='/content_items/" + item._id + "/edit'>Click to replace</a>" 
-     return first_time_edit_link.html_safe
-  end
   
   def index
   end
