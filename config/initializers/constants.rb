05f99f47bef502cc62a34c233580cdff54895a57
Dermot <dkil1972@gmail.com>
2014-07-01 06:40:43 +0100

    contact page is now mobile

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,7 +1,7 @@
 module Constants
   PER_PAGE = 5
   ADDRESS = "46 Gray's Inn Rd, London, WC1X 8LP"
-  TELL = "+44 3069 990496"
+  TELL = "+44 203 6371074"
   FAX = "+44 3069 990496"
   EMAIL = "office@nujob.com"
   MAIL_TO = "admin@nujob.co.uk"
