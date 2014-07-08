5a8f9ff0d6490508e29823d337503461c2ec63ed
Dermot <dkil1972@gmail.com>
2014-07-08 07:49:10 +0100

    tidied up the layout on the happy hire, changed the email address

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -3,7 +3,7 @@ module Constants
   ADDRESS = "46 Gray's Inn Rd, London, WC1X 8LP"
   TELL = "+44 203 6371074"
   FAX = "+44 3069 990496"
-  EMAIL = "office@nujob.com"
+  EMAIL = "admin@nujob.com"
   MAIL_TO = "admin@nujob.co.uk"
   OSMEK_API = "8ytjaEzRiIrPl1meNC5UTfpnhO7vWKYB"
   SECTION_ID = "8948"
