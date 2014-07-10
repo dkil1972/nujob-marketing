83587bfb9a70ab2542f71eecebb8c51d1db42b9c
Andy Davis <me@andy-davis.co.uk>
2014-07-10 13:41:27 +0100

    pete image set to round and imageil address corrected to .co.uk

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,9 +1,9 @@
 module Constants
   PER_PAGE = 5
-  ADDRESS = "46 Gray's Inn Rd, London, WC1X 8LP"
+  ADDRESS = ""
   TELL = "+44 203 6371074"
-  FAX = "+44 3069 990496"
-  EMAIL = "admin@nujob.com"
+  FAX = ""
+  EMAIL = "admin@nujob.co.uk"
   MAIL_TO = "admin@nujob.co.uk"
   OSMEK_API = "8ytjaEzRiIrPl1meNC5UTfpnhO7vWKYB"
   SECTION_ID = "8948"
