78b4dbb78742ea964911c1a21e99cf8387c30f68
Dermot <dkil1972@gmail.com>
2015-01-26 11:57:56 +0000

    linking to the tour from the products page

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ NujobMarketing::Application.routes.draw do
   get '/about/' => "home#about"
   get '/products/' => "home#products"
   get "/testimonials" => "home#testimonials"
-  get "/tour" => "home#tour"
+  get "/tour" => "products#tour"
   get "/pricing" => "home#pricing"
   get "/contact" => "home#contact"
   get "/careers" => "home#careers"
